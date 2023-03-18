d9733a15c981377faa190bd02b8bd6bf779b9493
UP2021407 <up2021407@myport.ac.uk>
2023-03-18 17:48:58 +0000

    Added timeout function to for loop

--- WILL-CODE/main.js
+++ WILL-CODE/main.js
@@ -1,10 +1,10 @@
 import * as crypto from "crypto";
-
 import fetch from 'node-fetch'
 import { promises as fs } from 'fs';
+import { time } from "console";
 
 // utility fn to load a json file (oh how it was easy when we could just `require`)
-const load = async (file) => JSON.parse( await fs.readFile(file, "utf8") );
+const load = async (file) => JSON.parse(await fs.readFile(file, "utf8"));
 
 const data = await load('./config/data.json');
 const options = { headers: await load('./config/headers.json') };
@@ -12,26 +12,23 @@ const options = { headers: await load('./config/headers.json') };
 const host = 'https://t-esbprep-apigw.port.ac.uk';
 const basePath = '/gateway/BiDataWarehouseSitsCourseModuleAssessmentAPI';
 
-
-async function getCourses() {
-
-  const route = `courses/${data.depts.comp}/${data.years.sep22}`;
+async function getTutorial(studentID) {
+  const route = `student/SOC/tutorial/${studentID}/${data.years.sep22}`;
   const url = `${host}${basePath}/${route}`;
 
+  console.log(url);
+  
   const result = await fetch(url, options);
   return await result.json();
 }
 
-async function moduleAssesment() {
-
-  const route = `courses/${data.depts.comp}/${data.years.sep22}`;
-  const url = `${host}${basePath}/${route}`;
-
-  const result = await fetch(url, options);
-  return await result.json();
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const courses = await getCourses();
-const moduleAsses = await moduleAssesment();
-console.log(moduleAsses);
-console.log(courses);
+for (const student in data.studentID){
+  await timeout(500);
+  const tutorial = await getTutorial(data.studentID[student]);
+  console.log(tutorial);
+}
+
